d7a3341a0b84b5c85f40d5399997e2262510eaf8
alm818 <alm818@nyu.edu>
2022-09-06 22:35:49 +0400

    ssds and tpch

---
+++ resource/ssds.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import wget, os, bz2
+import multiprocessing as mp
+AP_counts = [60, 126]
+counts = sum(AP_counts)
+AP_sign = ["", "2"]
+
+
+# In[2]:
+
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+# In[3]:
+
+
+folder = "ssds/"
+create_folder(folder)
+
+
+# In[4]:
+
+
+AP_name = "sqlApogee{}Object_{:04d}.csv.bz2"
+
+for ii in range(2):
+    for i in range(1, AP_counts[ii]+1):
+        name = AP_name.format(AP_sign[ii], i)
+        url = "https://dr17.sdss.org/sas/dr17/casload/apCSV/target/" + name
+        download = True
+        file = folder + name
+        if os.path.exists(file):
+            download = False
+        if download:
+            wget.download(url, out=file)
+        print("\nFinished downloading {}".format(file))
+
+
+# In[5]:
+
+
+config = {}
+config_file = open("../config.txt", 'r')
+for l in config_file:
+    l = l.strip()
+    if len(l) > 0 and l[0] != "#":
+        key, value = [x.strip() for x in l.split("=")]
+        config[key] = value
+
+import psycopg
+table_name = "ssds"
+
+# Connect to an existing database
+with psycopg.connect("dbname={} host={} port={} user={} password={}".format(config["database"], config["hostname"], config["port"], config["username"], config["password"])) as conn:
+    with conn.cursor() as cur:
+        cur.execute("""DROP TABLE IF EXISTS {}""".format(table_name))
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS {} (
+            {} BIGINT,
+            j DOUBLE PRECISION,
+            h DOUBLE PRECISION,
+            k DOUBLE PRECISION,
+            tmass_prox DOUBLE PRECISION)
+        """.format(table_name, config["id_column"]))
+        conn.commit()
+
+
+# In[6]:
+
+
+global_size = mp.Value("i", 0)
+global_index = mp.Value("i", 0)
+def generating_data(global_size, global_index):
+    while True:
+        local_index = -1
+        ap_version, ap_index = -1, -1
+        with global_index.get_lock():
+            local_index = global_index.value
+            global_index.value += 1
+        if local_index >= counts:
+            break
+        for count in AP_counts:
+            ap_version += 1
+            if local_index < count:
+                ap_index = local_index + 1
+            else:
+                local_index -= count
+        cols = ["j", "h", "k", "tmass_prox"]
+        file_name = folder + AP_name.format(AP_sign[ap_version], ap_index)
+        f = bz2.open(file_name, mode='rt')
+        size = -1
+        col_indices = [-1 for i in range(len(cols))]
+        data = []
+        for l in f:
+            tokens = l.split(',')
+            if size == -1:
+                for i, col in enumerate(cols):
+                    for j, token in enumerate(tokens):
+                        if col == token:
+                            col_indices[i] = j
+            else:
+                vals = [float(tokens[j]) for j in col_indices]
+                data.append(vals)
+            size += 1
+        f.close()
+        start_index = -1
+        with global_size.get_lock():
+            start_index = global_size.value+1
+            global_size.value += size
+            print("File:", file_name, "Start_index:", start_index, "Size:", size)
+        with psycopg.connect("dbname={} host={} port={} user={} password={}".format(config["database"], config["hostname"], config["port"], config["username"], config["password"])) as conn:
+            with conn.cursor() as cur:
+                with cur.copy("COPY ssds ({}, {}) FROM STDIN".format(config["id_column"], ','.join(cols))) as copy:
+                    for i, row in enumerate(data):
+                        copy.write_row([start_index + i] + row)
+            conn.commit()
+        print("File:", file_name, "finished inserting", size, "rows")
+
+
+# In[ ]:
+
+
+ps = [mp.Process(target = generating_data, args = (global_size, global_index)) for i in range(int(config["physical_core"]))]
+for p in ps:
+    p.start()
+for p in ps:
+    p.join()
+
+
+# In[7]:
+
+
+print("Starts indexing ssds")
+with psycopg.connect("dbname={} host={} port={} user={} password={}".format(config["database"], config["hostname"], config["port"], config["username"], config["password"])) as conn:
+    with conn.cursor() as cur:
+        cur.execute("""ALTER TABLE {} ADD PRIMARY KEY ({})""".format(table_name, config["id_column"]))
+        conn.commit()
+print("Finished indexing ssds")
+
+
+# In[ ]:
+
+
+
+
---
+++ resource/tpch.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+import os, subprocess
+import multiprocessing as mp
+
+
+# In[31]:
+
+
+config = {}
+config_file = open("../config.txt", 'r')
+for l in config_file:
+    l = l.strip()
+    if len(l) > 0 and l[0] != "#":
+        key, value = [x.strip() for x in l.split("=")]
+        config[key] = value
+
+import psycopg
+table_name = "tpch"
+
+# Connect to an existing database
+with psycopg.connect("dbname={} host={} port={} user={} password={}".format(config["database"], config["hostname"], config["port"], config["username"], config["password"])) as conn:
+    with conn.cursor() as cur:
+        cur.execute("""DROP TABLE IF EXISTS {}""".format(table_name))
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS {} (
+            {} BIGINT,
+            quantity DOUBLE PRECISION,
+            price DOUBLE PRECISION,
+            discount DOUBLE PRECISION,
+            tax DOUBLE PRECISION)
+        """.format(table_name, config["id_column"]))
+        conn.commit()
+
+
+# In[32]:
+
+
+if not os.path.exists("tpch-kit"):
+    result = subprocess.run("""
+        git clone https://github.com/gregrahn/tpch-kit.git;
+        cd tpch-kit/dbgen;
+        make MACHINE=LINUX DATABASE=POSTGRESQL
+    """, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if result.returncode == 0:
+        print("git clone and make succesffully")
+    else:
+        print("Error when executing git clone and make")
+
+
+# In[35]:
+
+
+sf = 300
+thread = 44
+global_size = mp.Value("i", 0)
+def generating_data(global_size, sf, thread):
+    file_name = "tpch-kit/dbgen/lineitem.tbl.{}".format(thread)
+    if not os.path.exists(file_name):
+        result = subprocess.run("""
+            cd tpch-kit/dbgen; 
+            ./dbgen -s {} -S {} -C {} -T L
+        """.format(sf, thread, sf), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        if result.returncode != 0:
+            print("Error when executing dbgen for thread", thread)
+    if os.path.exists(file_name):
+        size = 0
+        data = []
+        for row in open(file_name):
+            q, p, d, t = [float(x) for x in row.split("|")[4:8]]
+            d *= p
+            t *= p
+            data.append([q, p, d, t])
+            size += 1
+        start_index = -1
+        with global_size.get_lock():
+            start_index = global_size.value+1
+            global_size.value += size
+            print("Thread:", thread, "Start_index:", start_index, "Size:", size)
+        with psycopg.connect("dbname={} host={} port={} user={} password={}".format(config["database"], config["hostname"], config["port"], config["username"], config["password"])) as conn:
+            with conn.cursor() as cur:
+                with cur.copy("COPY tpch ({}, quantity, price, discount, tax) FROM STDIN".format(config["id_column"])) as copy:
+                    for i, row in enumerate(data):
+                        copy.write_row([start_index + i] + row)
+            conn.commit()
+        print("Thread", thread, "finished inserting", size, "rows")
+
+
+# In[29]:
+
+
+ranges = [(1, sf//2), (sf//2 + 1, sf)]
+for a,b in ranges: 
+    ps = [mp.Process(target = generating_data, args = (global_size, sf, i)) for i in range(a, b+1)]
+    for p in ps:
+        p.start()
+    for p in ps:
+        p.join()
+
+
+# In[37]:
+
+
+print("Starts indexing tpch")
+with psycopg.connect("dbname={} host={} port={} user={} password={}".format(config["database"], config["hostname"], config["port"], config["username"], config["password"])) as conn:
+    with conn.cursor() as cur:
+        cur.execute("""ALTER TABLE {} ADD PRIMARY KEY ({})""".format(table_name, config["id_column"]))
+        conn.commit()
+print("Finished indexing tpch")
+
+
+# In[ ]:
+
+
+
+
