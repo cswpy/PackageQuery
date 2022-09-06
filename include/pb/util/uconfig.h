1897484940ea2228f2bfcec901c540ef06b11730
alm818 <alm818@nyu.edu>
2022-09-07 00:26:57 +0400

    configs

--- include/pb/util/uconfig.h
+++ include/pb/util/uconfig.h
@@ -1,24 +1,66 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+
+#include "umisc.h"
 
 using std::string;
+using std::unordered_map;
 
-const string kProjectHome = "/home/alm818/PackageQuery";
-//const string kProjectHome = "C:/Users/xuana/Desktop/VisualStudioCode/PackageQuery";
+static int readConfigs(unordered_map<string, string>& configs){
+  char separator = '/';
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) == NULL) {
+    perror("getcwd() error");
+    return 1;
+  }
+  string project_home (cwd);
+  project_home = project_home.substr(0, project_home.find_last_of(separator));
+  configs["project_home"] = project_home;
 
-constexpr int kPCore = 80;
-constexpr int kLCore = 160;
-constexpr int kMaxMultiColumnIndexes = 2;
-constexpr int kPgPort = 5433;
-constexpr int kPrecision = 20;
-constexpr long long kInMemorySize = 1000000; // In Memory Size for all cores in term of number of tuples
+  std::fstream config_file; config_file.open(project_home + separator + "config.txt", std::ios::in);
+  if (config_file.is_open()){
+    string line;
+    while (getline(config_file, line)){
+      line = trim(line);
+      if (line.length() > 0 && line[0] != '#'){
+        auto pos = line.find('=');
+        if (pos != string::npos){
+          string key = line.substr(0, pos);
+          string value = line.substr(pos+1);
+          configs[trim(key)] = trim(value);
+        }
+      }
+    }
+    config_file.close();
+  }
+  // for (auto p : configs){
+  //   std::cout << p.first << " " << p.second << "\n";
+  // }
+  return 0;
+}
 
-const string kPgUser = "alm818";
-const string kPgPassword = "";
-const string kPgHostaddr = "127.0.0.1";
+static unordered_map<string, string> configs;
+static int _ = readConfigs(configs);
 
-const string kId = "id"; // Must not equal to "tid"
+const string kProjectHome = configs["project_home"];
+
+static const int kPCore = stoi(configs["physical_core"]);
+static const int kLCore = stoi(configs["logical_core"]);
+static const int kMaxMultiColumnIndexes = stoi(configs["max_multicolumn_indexes"]);
+static const int kPgPort = stoi(configs["port"]);
+static const int kPrecision = stoi(configs["precision"]);
+static const long long kInMemorySize = stoll(configs["in_memory_size"]); // In Memory Size for all cores in term of number of tuples
+
+const string kPgUser = configs["user"];
+const string kPgPassword = configs["password"];
+const string kPgHostaddr = configs["hostname"];
+
+const string kId = configs["id_column"]; // Must not equal to "tid"
 const string kStatTable = "dlv_stats";
 const string kPartitionTable = "dlv_partitions";
 const string kNullLiteral = "null";
---
+++ runnable/main_config.cpp
@@ -0,0 +1,20 @@
+#include "pb/util/udeclare.h"
+#include "pb/util/uconfig.h"
+
+using namespace pb;
+
+int main(){
+  // int N = 20;
+  // int d = 5;
+  // VectorXd array (N);
+  // std::default_random_engine gen;
+  // gen.seed(std::chrono::system_clock::now().time_since_epoch().count());
+  // std::uniform_real_distribution<double> dist(0.0,1.0);
+  // for (int i = 0; i < N; i ++) array(i) = dist(gen);
+  // VectorXd sorted_array = array;
+  // sort(sorted_array.begin(), sorted_array.end());
+  // VectorXd delims (d-1);
+  // for (int i = 0; i < d-1; i ++) delims(i) = sorted_array((i+1)*(N/d));
+  // print(sorted_array);
+  // print(delims);
+}
